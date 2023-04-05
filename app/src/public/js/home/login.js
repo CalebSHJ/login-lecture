f4f7b3485f42ad9b09d97091ebe29c4fac90cd9a
Seung Hun Jung <littlelight4jx@gmail.com>
2023-04-05 15:57:04 -0700

    login js update

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,1 +1,15 @@
-console.log("hello");
+"use strict";
+
+const id = document.querySelector("#id"),
+  password = document.querySelector("#password"),
+  loginBtn = document.querySelector("#loginBtn");
+
+loginBtn.addEventListener("click", function login(){
+  const req = {
+    id: id.value,
+    password: password.value,
+  };
+  console.log(req)
+});
+
+ 
