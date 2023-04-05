d89958a486cc73664a5712b39cff8ac648c09644
Seung Hun Jung <littlelight4jx@gmail.com>
2023-04-05 15:32:59 -0700

    front js add

--- app/app.js
+++ app/app.js
@@ -25,5 +25,6 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use("/", home); //middleware를 등록하는 method
+app.use(express.static(`${__dirname}/src/public`));
 
 module.exports = app;
