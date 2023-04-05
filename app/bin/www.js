1273f554a0eece3220eea401af607c2977e21d21
Seung Hun Jung <littlelight4jx@gmail.com>
2023-04-05 15:39:06 -0700

    add nodemon

--- app/bin/www.js
+++ app/bin/www.js
@@ -1,7 +1,7 @@
 "use strict"
 
 const app = require("../app");
-const PORT = 3000;
+const PORT = 3000; //nodemon ./bin/www.js를 사용하면 사용 중인 서버라는 메세지가 나오지 않도록 서버를 자동으로 끄고 다시 시작함.
 
 app.listen(PORT, () => {
   console.log("server starts");
